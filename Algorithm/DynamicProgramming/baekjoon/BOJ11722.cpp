95b9784f48e86dff634b8552fc7ff2382ac37663
Daeuning <happyjde3@gmail.com>
2024-08-29 10:25:59 +0900

    BOJ 11722

---
+++ Algorithm/DynamicProgramming/baekjoon/BOJ11722.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N;
+int arr[1002];
+int D[1002];
+
+int main(void)
+{
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> arr[i];
+	}
+
+	D[1] = 1;
+	
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = i - 1; j >= 1; j--)
+		{
+			if (arr[j] > arr[i]) D[i] = max(D[i], D[j] + 1);
+			else D[i] = max(D[i], 1);
+		}
+	}
+
+	cout << *max_element(D, D + N + 1);
+
+	return 0;
+}
